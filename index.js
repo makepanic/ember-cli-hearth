8461a27190a457639b20874058aa4a7a9e8a6b01
Christian <me@rndm.de>
2016-03-19 22:24:21 +0100

    moves hearth from command to express middleware

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 /* jshint node: true */
 'use strict';
 
+const express = require('express');
+
+const hearth = require('./lib/api');
+const WS = require('./lib/models/ws');
+
 module.exports = {
   name: 'ember-cli-hearth',
 
-  includedCommands: function() {
-    return {
-      'hearth':       require('./lib/commands/hearth')
+  serverMiddleware(config){
+    const options = config.options;
+    const app = config.app;
+    const socketPort = options.port + 1;
+    const ui = options.ui;
+
+    app.use('/hearth', express.static('web'));
+
+    const ws = new WS({
+      port: socketPort,
+      project: this.project,
+      host: '::',
+      ui
+    });
+
+    const mapping = {
+      'hearth-ready': 'emitProjects',
+      'hearth-run-cmd': 'runCmd',
+      'hearth-kill-cmd': 'killCmd'
     };
+
+    Object.keys(mapping).forEach((evName) => {
+      ws.on(evName, (ws, data) => {
+        console.log('ipc', evName, ...data);
+        hearth[mapping[evName]](ws, ...data);
+      });
+    });
   }
 };
